8d0fad17236ecbcf656db917c1d315ac0c651495
Willa2023 <suanxiaoyi2865@gmail.com>
2024-02-01 23:37:16 +1300

    fix data 2

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,7 @@
 //data.js
 const { sql } = require("@vercel/postgres");
 
-export async function fetchArticleByTopic(topic) {
+export async function fetchArticleByTopic({ topic }) {
   try {
     const data = await sql`
       SELECT *
